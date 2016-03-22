642952ca21540939afff9404675c9169c1b71c93
Andrew Carr <aacarr5@gmail.com>
2016-03-22 18:59:52 -0500

    Index test passes

--- spec/views/welcome/index_view_spec.rb
+++ spec/views/welcome/index_view_spec.rb
@@ -4,8 +4,7 @@ describe "welcome/index.html.erb" do
 	it "displays the mission statement of Black Tech Mecca" do 
 		render
 
-		expect(rendered).to match /Black Tech Mecca/
-		expect(rendered).to match /BTM connects the black community by sharing information and resources,
-		lowering  barriers, and exchange narratives/
+		expect(rendered.match /Black Tech Mecca/).to be_a MatchData
+		expect(rendered.match /BTM connects the black community by sharing information and resources, lowering barriers, and exchange narratives/).to be_a MatchData
 	end
 end
